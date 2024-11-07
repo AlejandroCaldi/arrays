1ef8537517c3d074ccd20cdd76e2d0fdb116b459
curso07 <alejandro.cald@gmail.com>
2024-11-07 11:53:32 +0100

    Agergado de la funcioanlidad para que el usuario pueda definir el largo del aray

--- src/main/java/es/santander/ascender/proyecto04/App.java
+++ src/main/java/es/santander/ascender/proyecto04/App.java
@@ -1,5 +1,7 @@
 package es.santander.ascender.proyecto04;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  */
@@ -7,8 +9,15 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Bienvenido al Ejercicio de Manipulación de Arrays");
         Arrays arra = new Arrays();
+        
+        Scanner scanner = new Scanner(System.in);
 
-        int[] arrayOriginal = arra.generarArrayIncremental(99, true);
+        // Reading a String input
+        System.out.print("Ingrese el largo del array original: ");
+        int largoArray = scanner.nextInt();
+        scanner.close();
+
+        int[] arrayOriginal = arra.generarArrayIncremental(largoArray-1, true);
         int[] arrayRevertido = arra.revertirArrayIncremental(arrayOriginal, true);
 
         System.out.println(arrayRevertido);
