ca5574ae078f1086fae74e8b02eb7ce471b227c4
curso07 <alejandro.cald@gmail.com>
2024-11-08 09:45:19 +0100

    agregado de testeo de assertNotNulls

--- src/test/java/es/santander/ascender/proyecto04/ArreglosTest.java
+++ src/test/java/es/santander/ascender/proyecto04/ArreglosTest.java
@@ -1,6 +1,7 @@
 package es.santander.ascender.proyecto04;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +14,7 @@ public class ArreglosTest {
         int[] esperado = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         int limite = esperado.length;
         int[] prueba = cut.generarArrayIncremental(limite, false);
+        assertNotNull(prueba, "El array recibido es nulo");
         for (int i = 0; i <= limite - 1; i++) {
 
             assertEquals(prueba[i], esperado[i], "En el índice " + i + " del array no coinciden los valores");
@@ -26,6 +28,7 @@ public class ArreglosTest {
         int[] esperado = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
         int limite = esperado.length;
         int[] prueba = cut.revertirArrayIncremental(esperado, false);
+        assertNotNull(prueba, "El array recibido es nulo");
         for (int i = 0; i <= limite - 1; i++) {
 
             assertEquals(prueba[i], esperado[i], "En el índice " + i + " del array no coinciden los valores");
