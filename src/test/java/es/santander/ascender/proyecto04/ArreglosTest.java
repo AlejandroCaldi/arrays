64bda372c76ecd43bbaf686fbd02da9d39363670
curso07 <alejandro.cald@gmail.com>
2024-11-08 12:30:19 +0100

    Cambio a assertArrayEquals

--- src/test/java/es/santander/ascender/proyecto04/ArreglosTest.java
+++ src/test/java/es/santander/ascender/proyecto04/ArreglosTest.java
@@ -1,5 +1,6 @@
 package es.santander.ascender.proyecto04;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -11,24 +12,27 @@ public class ArreglosTest {
 
     @Test
     public void testGenerarArrayIncremental() {
-        int[] esperado = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int[] esperado = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; // no harìa falta el new int[]
         int limite = esperado.length;
         int[] prueba = cut.generarArrayIncremental(limite, false);
         assertNotNull(prueba, "El array recibido es nulo");
-        for (int i = 0; i <= limite - 1; i++) {
 
-            assertEquals(prueba[i], esperado[i], "En el índice " + i + " del array no coinciden los valores");
+        assertArrayEquals(esperado, prueba);
+        // for (int i = 0; i <= limite - 1; i++) {
 
-        }
+        //     assertEquals(prueba[i], esperado[i], "En el índice " + i + " del array no coinciden los valores");
+
+        // }
 
     }
 
     @Test
     public void testRevertirArrayIncremental() {
-        int[] esperado = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+        int[] esperado = { 10, 9, 8, 7, 6, 5, 3, 3, 2, 1, 0 }; // no harìa falta el new int[]
         int limite = esperado.length;
         int[] prueba = cut.revertirArrayIncremental(esperado, true);
         assertNotNull(prueba, "El array recibido es nulo");
+        assertArrayEquals(esperado, prueba);
         for (int i = 0; i <= limite - 1; i++) {
 
             assertEquals(prueba[i], esperado[(limite -1) -i], "En el índice " + i + " del array no coinciden los valores");
